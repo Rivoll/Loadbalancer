8d6420f0715a0183955006dd00a57d2b52cc80ec
Rivoll <patrick.keita@outlook.com>
2020-04-30 10:36:21 +0200

    Create update_eip.sh

---
+++ update_eip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+REGION=`cat instance_region`
+aws ec2 describe-addresses $REGION --filters 'Name=tag:Name,Values=lb' --query Addresses[].NetworkInterfaceId[] > eip
+
+if [[ `cat eip` == "[]"  ]] ;
+then
+        echo "cle non alloue. Allocation en cours ..."
+        ALLOC_ID=`aws ec2 describe-addresses --region $REGION --filters 'Name=tag:Name,Values=lb' --query Addresses[].AllocationId --output text`
+        INSTANCE_ID=`cat instance_id`
+         aws ec2 associate-address --region eu-west-2 --instance-id $INSTANCE_ID --allocation-id $ALLOC_ID
+        echo "alloc id = $ALLOC_ID, instance id = $INSTANCE_ID"
+else
+        echo "cle deja alloue"
+fi
